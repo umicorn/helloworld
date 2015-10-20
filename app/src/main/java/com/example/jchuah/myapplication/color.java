8ff9e2f400e2efdfd73f359ec74b5492f27c8956
umicorn <polibo.felix@gmail.com>
2015-10-20 12:33:34 -0500

    allowed to change screen when fear of the unknown is clicked :)

--- app/src/main/java/com/example/jchuah/myapplication/color.java
+++ app/src/main/java/com/example/jchuah/myapplication/color.java
@@ -1,42 +1,22 @@
 package com.example.jchuah.myapplication;
 
-import android.content.Intent;
-import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-
-    static String tag = "com.example.jchuah.myapplication";
-    Bundle sendBundle = new Bundle();
+public class color extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_color);
     }
-
-
-
-
-    public void onNextClick(View source) {
-       //Toast.makeText(this, "Howdy", Toast.LENGTH_LONG).show();
-        Log.i("tag", "Next Button clicked");
-        Intent colorIntent = new Intent(this, color.class) ;
-        startActivity(colorIntent, sendBundle);
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_color, menu);
         return true;
     }
 
